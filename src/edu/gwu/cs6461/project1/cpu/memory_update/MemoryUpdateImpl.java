8ea571a5e6d3ba24aab3410aad3917c9d2c649b6
duankebo <duankebo@gmail.com>
2018-02-05 17:00:30 -0500

    hw1

---
+++ src/edu/gwu/cs6461/project1/cpu/memory_update/MemoryUpdateImpl.java
@@ -0,0 +1,72 @@
+package edu.gwu.cs6461.project1.cpu.memory_update;
+
+import edu.gwu.cs6461.project1.cpu.Instruction;
+import edu.gwu.cs6461.project1.cpu.InstructionType;
+import edu.gwu.cs6461.project1.cpu.Registers;
+import edu.gwu.cs6461.project1.cpu.RegistersImpl;
+import edu.gwu.cs6461.project1.memory.Memory;
+import edu.gwu.cs6461.project1.memory.MemoryImpl;
+
+public class MemoryUpdateImpl implements MemoryUpdate{
+
+    static MemoryUpdateImpl instance;
+
+    private MemoryUpdateImpl() {
+        initialize();
+    }
+
+    static public MemoryUpdateImpl getInstance(){
+        if(instance == null){
+            instance = new MemoryUpdateImpl();
+        }
+        return instance;
+    }
+
+    void initialize() {
+
+    }
+
+    @Override
+    public short updateMemory(Instruction instruction) {
+        short value;
+        short addr = instruction.getValE();
+        short indirect = instruction.getI();
+
+        Memory memory = MemoryImpl.getInstance();
+        Registers registers = RegistersImpl.getInstance();
+        if(indirect != 0) {
+            addr = memory.getMemory(addr);
+        }
+
+        switch(instruction.getOpcode()) {
+            case InstructionType.LDR:
+            case InstructionType.LDX:
+                //valM <- M[valE] or
+                //valM <- M[M[valE]]
+                value = memory.getMemory(addr);
+                instruction.setValM(value);
+                break;
+
+            case InstructionType.STR:
+                //M[valE] <- R[r]
+                value = registers.getGPR(instruction.getR());
+                memory.setMemory(addr, value);
+                break;
+
+            case InstructionType.LDA:
+                //No operation
+                break;
+
+            case InstructionType.STX:
+                //M[valE] <- X[ix]
+                value = registers.getX(instruction.getIx());
+                memory.setMemory(addr, value);
+                break;
+
+            default:
+                break;
+
+        }
+        return 0;
+    }
+}
---
+++ src/edu/gwu/cs6461/project1/cpu/register_update/RegisterUpdateImpl.java
@@ -0,0 +1,64 @@
+package edu.gwu.cs6461.project1.cpu.register_update;
+
+import edu.gwu.cs6461.project1.cpu.Instruction;
+import edu.gwu.cs6461.project1.cpu.InstructionType;
+import edu.gwu.cs6461.project1.cpu.Registers;
+import edu.gwu.cs6461.project1.cpu.RegistersImpl;
+import edu.gwu.cs6461.project1.cpu.memory_update.MemoryUpdate;
+
+public class RegisterUpdateImpl implements RegisterUpdate{
+
+    static RegisterUpdateImpl instance;
+
+    private RegisterUpdateImpl() {
+        initialize();
+    }
+
+    static public RegisterUpdateImpl getInstance(){
+        if(instance == null){
+            instance = new RegisterUpdateImpl();
+        }
+        return instance;
+    }
+
+    void initialize() {
+
+    }
+    @Override
+    public short updateRegister(Instruction instruction) {
+        short value;
+        Registers registers = RegistersImpl.getInstance();
+        switch(instruction.getOpcode()) {
+            case InstructionType.LDR:
+                //R[r] <- valM
+                value = instruction.getValM();
+                registers.setGPR(instruction.getR(), value);
+                break;
+
+            case InstructionType.STR:
+                //No operation
+                break;
+
+            case InstructionType.LDA:
+                //R[r] <- valE
+                value = instruction.getValE();
+                registers.setGPR(instruction.getR(), value);
+                break;
+
+            case InstructionType.LDX:
+                //x[ix] <- valM
+                value = instruction.getValM();
+                registers.setX(instruction.getIx(), value);
+                break;
+
+            case InstructionType.STX:
+                //No operation
+                break;
+
+            default:
+                break;
+
+        }
+        return 0;
+    }
+}
